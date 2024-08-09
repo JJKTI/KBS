a4c78f07feb8c355983cd1f0b35a236456cae44a
JJKTI <xodls0498@jj.ac.kr>
2024-08-09 15:47:34 +0900

    firtst commit

---
+++ js/main2.js
@@ -0,0 +1,31 @@
+// JS
+const mainMenuEl = document.querySelector('.main-menu');
+const menuBgEl = document.querySelector('.menu-bg');
+
+// console.log(mainMenuEl);
+// console.log(menuBgEl);
+
+mainMenuEl.addEventListener('mouseover',function(){
+    menuBgEl.classList.add('on');
+});
+mainMenuEl.addEventListener('mouseout',function(){
+    menuBgEl.classList.remove('on');
+});
+menuBgEl.addEventListener('mouseover',function(){
+    menuBgEl.classList.add('on');
+    mainMenuEl.classList.add('on');
+});
+menuBgEl.addEventListener('mouseout',function(){
+    menuBgEl.classList.remove('on');
+    mainMenuEl.classList.remove('on');
+});
+
+const mobileMenuEl = document.querySelector('.mobile-menu');
+const closeMenuEl = document.querySelector('.close');
+const mobileBoxEl = document.querySelector('.mobile-box');
+mobileMenuEl.addEventListener('click',function(){
+    mobileBoxEl.classList.add('show');
+});
+closeMenuEl.addEventListener('click',function(){
+    mobileBoxEl.classList.remove('show');
+});
